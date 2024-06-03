304dccf9f74a15dd0925e843317240526c683a42
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-06-03 09:47:28 +0200

    Job01 Finish.

---
+++ jour01/job01/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$platform = "LaPlateforme";
+echo $platform . "<br>";
+
+$vive = "Vive";
+$exclamation = "!";
+echo $vive . " " . $platform . " " . $exclamation . "<br>";
+
+$number = 6;
+echo $number . "<br>";
+
+$number += 4;
+echo $number . "<br>";
+
+$isTrue = true;
+echo ($isTrue ? 'true' : 'false') . "<br>";
+
+$isTrue = false;
+echo ($isTrue ? 'true' : 'false') . "<br>";
+?>
