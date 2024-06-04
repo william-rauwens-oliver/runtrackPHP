abce05b40316ad19ece2bef98fb2097eb1ecdffc
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-06-04 11:30:51 +0200

    Add Job06 Finish.

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Formulaire de Connexion</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de connexion</title>
 </head>
 <body>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
 
-        if ($username === "John" && $password === "Rambo") {
-            echo "Ce n'est pas ma guerre";
-        } else {
-            echo "Votre pire cauchemar";
-        }
+<form action="" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username"><br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = isset($_POST['username']) ? htmlspecialchars($_POST['username']) : '';
+    $password = isset($_POST['password']) ? htmlspecialchars($_POST['password']) : '';
+
+    if ($username === 'John' && $password === 'Rambo') {
+        echo "<p>ce n'est pas ma guerre</p>";
+    } else {
+        echo "<p>votre pire cauchemar</p>";
     }
-    ?>
+}
+?>
 
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" id="username" name="username" required><br>
-
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required><br>
-
-        <input type="submit" value="Se connecter">
-    </form>
 </body>
 </html>
