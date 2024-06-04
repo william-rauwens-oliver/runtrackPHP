bfda17e8d6816202f11c58b545003899aadabcce
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-06-04 11:31:23 +0200

    Add Job06 Finish.

--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire</title>
+    <title>Formulaire GET pour Nombre Pair ou Impair</title>
 </head>
 <body>
-    <?php
-    if (isset($_GET['nombre'])) {
-        $nombre = intval($_GET['nombre']);
-        if ($nombre % 2 === 0) {
-            echo "Nombre pair";
+
+<form action="" method="get">
+    <label for="nombre">Entrez un nombre:</label>
+    <input type="text" id="nombre" name="nombre"><br><br>
+    <input type="submit" value="Vérifier">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['nombre'])) {
+    $nombre = htmlspecialchars($_GET['nombre']);
+    if (is_numeric($nombre)) {
+        if ($nombre % 2 == 0) {
+            echo "<p>Nombre pair</p>";
         } else {
-            echo "Nombre impair";
+            echo "<p>Nombre impair</p>";
         }
+    } else {
+        echo "<p>Veuillez entrer un nombre valide.</p>";
     }
-    ?>
-    <form action="index.php" method="get">
-        <label for="nombre">Entrez un nombre :</label>
-        <input type="text" id="nombre" name="nombre">
-        <input type="submit" value="Valider">
-    </form>
+}
+?>
+
 </body>
 </html>
