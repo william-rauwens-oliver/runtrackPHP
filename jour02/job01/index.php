3ae8b496babbbc1d67eea90e98e300b74a6e39d2
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-06-04 11:20:58 +0200

    Add Job01 Finish.

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test GET Form</title>
+</head>
 <body>
 
-<form action="" method="get">
-  <label for="fname">Prénom:</label><br>
-  <input type="text" id="fname" name="fname"><br>
-  <label for="lname">Nom:</label><br>
-  <input type="text" id="lname" name="lname">
-  <input type="submit" value="Submit">
-</form> 
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $nombreArguments = count($_GET);
-    echo "Le nombre d'arguments GET est : " . $nombreArguments;
+if ($_SERVER["REQUEST_METHOD"] == "GET" && !empty($_GET)) {
+    $numberOfArguments = count($_GET);
+    echo "Nombre d'arguments GET : " . $numberOfArguments;
+} else {
+    echo "Aucun argument GET n'a été trouvé.";
 }
 ?>
+
+<form action="" method="get">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name"><br><br>
+    <label for="age">Age:</label>
+    <input type="text" id="age" name="age"><br><br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email"><br><br>
+    <input type="submit" value="Submit">
+</form>
 
 </body>
 </html>
